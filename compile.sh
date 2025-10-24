2f95f911f6d88b709950e12cd1465737c90ef9af
AtexBg <151460611+AtexBg@users.noreply.github.com>
2025-10-24 17:13:59 +0200

    Delete compile.sh

--- compile.sh
+++
@@ -1,7 +0,0 @@
-CODE="main.c"
-OUTPUT="wm-v4"
-OUTPUT_DIR="build"
-
-clear
-arm-linux-gnueabi-gcc -static -march=armv6 -o $OUTPUT_DIR/$OUTPUT $CODE
-echo [COMPILE.SH] "$OUTPUT" compiled for ARMv6 architecture if no compiler errors shown.
